6bf85caae590a01e890add679c8cee17778cf504
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 20:59:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@
 def map(source)
   i = 0
   array = []
-  while i < source.size do
-    array << (yield(source_array[i]))
+  while i < source.size
+    array << yield(source_array[i])
     i += 1
   end
   array
