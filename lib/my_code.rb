0c53efee5536845eb36008b6fdc676b78ae71cdf
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-14 20:57:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,5 @@
 # Your Code Here
-def my_own_map(source)
+def map(source)
   i = 0
   array = []
   while i < source.size do
@@ -9,18 +9,4 @@ def my_own_map(source)
   array
 end
 
-my_own_map(source_array) do |negative|
-  (negative * -1)
-end
 
-my_own_map(source_array) do |nothing|
-  nothing
-end
-
-my_own_map(source_array) do |double|
-  (double * 2)
-end
-
-my_own_map(source_array) do |square|
-  (square * 2)
-end
